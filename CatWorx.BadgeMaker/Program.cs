a4347063251a9038d4834326ead89d0f3a31e510
Jeremy's commits <jeremyhurst16@gmail.com>
2022-03-23 17:07:54 -0400

    simple fetch name using ReadLine and printing to console

---
+++ CatWorx.BadgeMaker/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+  static void Main(string[] args)
+  {
+    List<string> employees = new List<string>();
+    // Collect user values until the value is an empty string
+    while (true)
+    {
+      Console.WriteLine("Please enter a name: (leave empty to exit): ");
+      string input = Console.ReadLine();
+      // Break if the user hits ENTER without typing a name
+      if (input == "")
+      {
+        break;
+      }
+      employees.Add(input);
+    }
+    for (int i = 0; i < employees.Count; i++)
+    {
+      Console.WriteLine(employees[i]);
+    }
+  }
+}
