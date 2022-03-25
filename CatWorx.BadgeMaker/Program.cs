8d703569192c6393384744e568031fccdb20bd88
Jeremy's commits <jeremyhurst16@gmail.com>
2022-03-24 23:48:12 -0400

    added new util class, moved printemployee method to util, updated program.cs

--- CatWorx.BadgeMaker/Program.cs
+++ CatWorx.BadgeMaker/Program.cs
@@ -29,11 +29,7 @@
     }
     static void PrintEmployees(List<Employee> employees)
     {
-      for (int i = 0; i < employees.Count; i++)
-      {
-        string template = "{0,-10}\t{1,-20}\t{2}";
-        Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetName(), employees[i].GetPhotoUrl()));
-      }
+      Util.PrintEmployees(employees);
     }
     static void Main(string[] args)
     {
